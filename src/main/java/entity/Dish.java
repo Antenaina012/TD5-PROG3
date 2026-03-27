4e80519c3c1c989b747c0e4ebc71cc53db332934
Antenaina012 <hei.antenaina.7@gmail.com>
2026-03-27 22:34:12 +0300

    feat: finished Dish.java

--- src/main/java/entity/Dish.java
+++ src/main/java/entity/Dish.java
@@ -1,4 +1,21 @@
 package entity;
+import java.util.List;
 
 public class Dish {
+    private int id;
+    private String name;
+    private double salePrice;
+    private List<Ingredient> ingredients;
+
+    public Dish(int id, String name, double salePrice, List<Ingredient> ingredients) {
+        this.id = id;
+        this.name = name;
+        this.salePrice = salePrice;
+        this.ingredients = ingredients;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getSalePrice() { return salePrice; }
+    public List<Ingredient> getIngredients() { return ingredients; }
 }
